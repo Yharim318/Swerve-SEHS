170e76ced3b47cee2f6e9e416507ce073aade87f
Yharim318 <gradylikavec@gmail.com>
2024-02-05 16:59:41 -0500

    Base Shooter Code

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,7 +1,26 @@
 package frc.robot.subsystems;
 
-public class Intake {
-    Intake(int IntakeMotor1, int IntakeMotor2){
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.XboxController;
+
+public class Shooter {
+    CANSparkMax ShooterMotor1;
+    CANSparkMax ShooterMotor2;
+    XboxController cXboxControllerontroller;
+
+    public Shooter(int shooterMotor1, int shooterMotor2, int controller ){
+        ShooterMotor1 = new CANSparkMax(shooterMotor1, MotorType.kBrushless);
+        ShooterMotor2 = new CANSparkMax(shooterMotor2, MotorType.kBrushless);
+        cXboxControllerontroller = new XboxController(controller);
+
+        ShooterMotor1.setIdleMode(IdleMode.kBrake);
+        ShooterMotor2.setIdleMode(IdleMode.kBrake);
+    }
+    public void Shoot(){
+        ShooterMotor1.set(cXboxControllerontroller.getRightTriggerAxis() - cXboxControllerontroller.getLeftTriggerAxis());
+        ShooterMotor2.set(-(cXboxControllerontroller.getRightTriggerAxis() - cXboxControllerontroller.getLeftTriggerAxis()));
     }
 }
