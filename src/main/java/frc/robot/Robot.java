3c32041f54b63534ad41a470c10ec5188483f3b5
Yharim318 <gradylikavec@gmail.com>
2024-04-04 21:20:36 -0400

    Worlds

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -57,6 +57,12 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
 
+    /*
+     * if (robot.exists){
+     *  boolean AtWorlds = true;
+     * }
+     */
+
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
